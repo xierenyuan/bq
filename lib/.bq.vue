6cece3fa49f0b3e34c574c0d0d360a1324d32c9b
谢仁洪 <xierenyuan@qq.com>
2017-07-03 13:59:25 +0800

    add login && add create

---
+++ lib/.bq.vue
@@ -0,0 +1,23 @@
+<template>
+  <bq-page>
+
+  </bq-page>
+</template>
+<script>
+export default {
+  data() {
+    return {
+
+    }
+  },
+  created() {
+
+  }
+}
+</script>
+<style lang="scss">
+  @import 'scss/variables.scss';
+  @import 'scss/mixin.scss';
+</style>
+
+
